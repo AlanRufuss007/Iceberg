307e6ca6e83a0d0fe564f2800baef8c621f929d0
AlanRufuss007 <78805235+AlanRufuss007@users.noreply.github.com>
2021-02-12 15:08:37 +0530

    Add files via upload

---
+++ python day1.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 20
+sum = num1+num2
+print("sum of {0} and {1} is {2}".format(num1,num2,sum))
+
+num1 = input("Enter the number:")
+num2 = input("/n Enetr the number:")
+sum = float(num1)+float(num2)
+print("The sum of {0} and {1} is {2}".format(num1,num2,sum))
+
+a = 10
+b = 20
+maximum = max(a,b)
+print(maximum)
